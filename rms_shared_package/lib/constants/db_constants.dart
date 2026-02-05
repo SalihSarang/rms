c0191ff9631a7b2efd792a57b07062f7fe0d6a5d
Salih Sarang K <sarangsalih@gmail.com>
2026-02-05 12:52:06 +0530

    first commit

---
+++ rms_shared_package/lib/constants/db_constants.dart
@@ -0,0 +1,18 @@
+class MenuDbConstants {
+  static const String menu = 'menu';
+  static const String categories = 'categories';
+  static const String portionsAndPrice = 'portionsAndPrice';
+  static const String addOns = 'addOns';
+}
+
+class OrderDbConstants {
+  static const String orders = 'orders';
+}
+
+class StaffDbConstants {
+  static const String staff = 'staff';
+}
+
+class ManagerDbConstants {
+  static const String manager = 'manager';
+}
---
+++ rms_shared_package/lib/rms_shared_package.dart
@@ -0,0 +1,3 @@
+export 'enums/enums.dart';
+export 'models/menu_models/food_model/food_model.dart';
+export 'models/menu_models/category_model/category_model.dart';
